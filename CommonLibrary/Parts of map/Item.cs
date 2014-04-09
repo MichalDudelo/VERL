4b4ee87cf1273fa2f3b9ea78d5fe05723c4fca70
MichalDudelo <michal.dudelo@gmail.com>
2014-04-09 14:24:02 +0200

    Dodana funckja EQUALS

--- CommonLibrary/Parts of map/Item.cs
+++ CommonLibrary/Parts of map/Item.cs
@@ -6,8 +6,12 @@ namespace Common_Library.Parts_of_map
     [Serializable]
     public class Item
     {
-        enum type 
-        {one = 1, four = 4, eight = 8}
+
+        public override bool Equals(object obj)
+        {
+            Item i = obj as Item;
+            return (i == null) ? false : this.X == i.X && this.Y == i.Y;
+        }
 
         public int X;
         public int Y;
